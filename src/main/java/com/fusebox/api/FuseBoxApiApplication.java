dc7ecb8bca04da2d61e6eb21dbed0b7d0126a4d8
Darin Georgiev <georgiev.darin@gmail.com>
2026-05-07 11:54:28 +0300

    Fix configs

--- src/main/java/com/fusebox/api/FuseBoxApiApplication.java
+++ src/main/java/com/fusebox/api/FuseBoxApiApplication.java
@@ -3,9 +3,12 @@ package com.fusebox.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FuseBoxApiApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(FuseBoxApiApplication.class, args);
     }
 }
